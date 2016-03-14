3b1db84bc2c849faba80c9a4dc5b0b7528efed16
Hang Lu <hang.lu@hootsuite.com>
2016-03-14 10:51:41 -0700

    use 3rd party web service

--- app/controllers/CurrencyController.scala
+++ app/controllers/CurrencyController.scala
@@ -2,19 +2,25 @@ package controllers
 
 import org.joda.time.LocalDate
 import org.joda.time.format.DateTimeFormat
+import play.api.libs.json.JsValue
+import scala.concurrent.Future
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.api.libs.ws._
 import play.api.Configuration
 
-import javax.swing.text.DateFormatter
+import javax.inject.Inject
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's currency page.
  */
 @Singleton
-class CurrencyController @Inject() (configuration: Configuration) extends Controller {
+class CurrencyController @Inject() (configuration: Configuration, ws: WSClient) extends Controller {
 
   val currencyLogger = Logger(this.getClass)
   val dateFormatter = DateTimeFormat.forPattern("yyyy-MM-dd")
@@ -23,23 +29,29 @@ class CurrencyController @Inject() (configuration: Configuration) extends Contro
   /**
    * Display the date given
    */
-  def date(dateInput: String) = Authenticated {
+  def date(dateInput: String) = Authenticated.async {
 
     try {
-//      val initialDate: LocalDate = new LocalDate()
       val localDate: LocalDate = dateFormatter.parseLocalDate(dateInput)
       val currentDate: LocalDate = LocalDate.now()
 
       if (! localDate.isAfter(initialDate) && localDate.isBefore(currentDate)){
         currencyLogger.error(s"requested $dateInput is not in range")
-        BadRequest(s"requested $dateInput is not in range")
+        Future.successful(BadRequest(s"requested $dateInput is not in range"))
       } else {
-        Ok(s"TARDIS currency advisor for date $dateInput")
+        getFromFixIO(dateInput).map { json =>
+          currencyLogger.info(s"the returned json is $json")
+          Ok(s"TARDIS currency advisor for date $dateInput")
+        }
       }
     } catch {
-      case e: IllegalArgumentException => BadRequest(s"$dateInput is in wrong format")
+      case e: IllegalArgumentException => Future.successful(BadRequest(s"$dateInput is in wrong format"))
     }
 
   }
 
+  private def getFromFixIO(date: String): Future[JsValue] = {
+    val url = s"http://api.fixer.io/$date"
+    ws.url(url).get().map(_.json)
+  }
 }
